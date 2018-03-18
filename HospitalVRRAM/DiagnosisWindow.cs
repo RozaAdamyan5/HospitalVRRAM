49a4d5265e0a8269d04860588cb0c784229f8b62
Rafayel Mkrtchyan <rafayelmkrtchyan12@mail.ru>
2018-03-18 20:59:26 +0400

    pulling

--- HospitalVRRAM/DiagnosisWindow.cs
+++ HospitalVRRAM/DiagnosisWindow.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace HospitalWithDB
+namespace HospitalWithDB//
 {
     // Very low quality code
     public partial class DiagnosisWindow : Form
