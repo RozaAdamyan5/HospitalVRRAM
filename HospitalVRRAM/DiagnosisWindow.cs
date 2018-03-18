29a22bd2f7f7caecadb99c0e5594a87efd990e7b
Rafayel Mkrtchyan <rafayelmkrtchyan12@mail.ru>
2018-03-18 12:53:43 +0400

    I did nothing

--- HospitalVRRAM/DiagnosisWindow.cs
+++ HospitalVRRAM/DiagnosisWindow.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace HospitalWithDB
+namespace HospitalWithDB    //Rafo's been here
 {
     // Very low quality code
     public partial class DiagnosisWindow : Form
