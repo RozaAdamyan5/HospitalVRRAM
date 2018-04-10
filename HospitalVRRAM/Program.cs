06d03bdc97b0896bf77ab895ef8beba0a1abdb29
Roza <User@WIN-EK3DU9V7SB4>
2018-04-10 21:11:03 +0400

    .

--- HospitalVRRAM/Program.cs
+++ HospitalVRRAM/Program.cs
@@ -21,8 +21,9 @@ namespace HospitalVRRAM
             //Application.Run(new RegisterWindow());
             //Application.Run(new LoginWindow());
             //Application.Run(new DiagnosisWindow("Name Surname"));
-            Application.Run(new DoctorProfileWindow(new Doctor("Name", "Surname", 15, "blah", "aaa", 0, new DateTime(1991, 01, 10), 1000)));
-            Application.Run(new PatientProfileWindow(new Patient("Name", "Surname", 0, "099999999",  new DateTime(0,0,0))));
+            Application.Run(new DoctorProfileWindow(new Doctor("Name", "Surname", 15, 15, new DateTime(1991, 01, 10), 1000)));
+            Application.Run(new PatientProfileWindow(new Patient("Name", "Surname", 551, "fsafa", new DateTime(1999, 10, 10))));
+            //Application.Run(new AdminProfileWindow());
         }
     }
 }
